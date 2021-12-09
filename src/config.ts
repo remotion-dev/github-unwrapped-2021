b30d657568ab17576a50c9477ab50f9d42e9b45e
Jonny Burger <jonathanburger11@gmail.com>
2021-12-09 17:18:04 +0100

    main

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ import { AwsRegion } from "@remotion/lambda";
 
 export const AWS_REGION: AwsRegion = "us-east-1";
 export const functionName = "remotion-render-cjaha075u0";
-export const COMP_NAME = "starred";
+export const COMP_NAME = "main";
 export const SITE_ID = "wrapped";
