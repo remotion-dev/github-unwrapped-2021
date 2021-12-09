b27c2b91fc50ac8f1749c11e29362e33df722ac6
Jonny Burger <jonathanburger11@gmail.com>
2021-12-09 17:06:10 +0100

    better

--- remotion/get-user-local.ts
+++ remotion/get-user-local.ts
@@ -1,9 +1,6 @@
 export const getUserLocal = async (username: string) => {
-  const res = await fetch(`https://www.githubwrapped.com/api/stats`, {
-    method: "post",
-    body: JSON.stringify({
-      username,
-    }),
-  });
+  const res = await fetch(
+    `https://www.githubwrapped.com/api/stats/${username}`
+  );
   return res.json();
 };
