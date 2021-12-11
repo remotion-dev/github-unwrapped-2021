1acd8e7829faad1d1468ec6ba8979037b4b56357
Jonny Burger <jonathanburger11@gmail.com>
2021-12-11 13:35:28 +0100

    server side

--- remotion/font.ts
+++ remotion/font.ts
@@ -1,13 +1,15 @@
 import { continueRender, delayRender } from "remotion";
 
-const font = new FontFace(
-  "Jelle",
-  "url(https://jonnyburger.s3.eu-central-1.amazonaws.com/Jellee-Bold.woff2)"
-);
-const handle = delayRender();
-font.load().then(() => {
-  document.fonts.add(font);
-  continueRender(handle);
-});
+if ("FontFace" in window) {
+  const font = new FontFace(
+    "Jelle",
+    "url(https://jonnyburger.s3.eu-central-1.amazonaws.com/Jellee-Bold.woff2)"
+  );
+  const handle = delayRender();
+  font.load().then(() => {
+    document.fonts.add(font);
+    continueRender(handle);
+  });
+}
 
-export const getFont = () => font;
+export const getFont = () => null;
