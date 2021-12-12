0ac571cbc884e20b3543965b07feb05b86209145
Jonny Burger <jonathanburger11@gmail.com>
2021-12-13 00:35:23 +0100

    force rehydrate

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -21,6 +21,14 @@ export async function getStaticPaths() {
   return { paths: [], fallback: true };
 }
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 const iosSafari = () => {
   if (typeof window === "undefined") {
     return false;
@@ -162,165 +170,167 @@ export default function User(props: {
   }
 
   return (
-    <div ref={ref}>
-      <Head>
-        <title>
-          {username}
-          {"'"}s #GithubWrapped
-        </title>
-        <meta property="og:title" content="My page title" key="title" />
-        <meta property="og:image" content="/flash.png" />
-        <meta
-          name="description"
-          content={`My coding 2021 in review. Get your own personalized video as well!`}
-        />
-        <link rel="icon" href="/fav.png" />
-      </Head>
-      <div style={abs}>
-        <div style={container}>
-          <header style={style}>
-            <br></br>
-            <br></br>
-            <h1 style={title}>Here is your #GithubWrapped!</h1>
-            <h3 style={subtitle}>@{username}</h3>
-            <br></br>
-            {user ? (
-              <div
-                style={{
-                  position: "relative",
-                }}
-              >
-                <Player
-                  ref={player}
-                  component={Main}
-                  compositionHeight={1080}
-                  compositionWidth={1080}
-                  durationInFrames={990}
-                  fps={30}
+    <SafeHydrate>
+      <div ref={ref}>
+        <Head>
+          <title>
+            {username}
+            {"'"}s #GithubWrapped
+          </title>
+          <meta property="og:title" content="My page title" key="title" />
+          <meta property="og:image" content="/flash.png" />
+          <meta
+            name="description"
+            content={`My coding 2021 in review. Get your own personalized video as well!`}
+          />
+          <link rel="icon" href="/fav.png" />
+        </Head>
+        <div style={abs}>
+          <div style={container}>
+            <header style={style}>
+              <br></br>
+              <br></br>
+              <h1 style={title}>Here is your #GithubWrapped!</h1>
+              <h3 style={subtitle}>@{username}</h3>
+              <br></br>
+              {user ? (
+                <div
                   style={{
-                    ...layout,
-                    boxShadow: "0 0 10px " + transparentize(0.8, BASE_COLOR),
-                    borderRadius: 10,
-                    overflow: "hidden",
-                  }}
-                  inputProps={{
-                    stats: user,
-                    enableDecoration: false,
-                  }}
-                ></Player>
-                <AbsoluteFill
-                  style={{
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                    display: "flex",
-                    cursor: "pointer",
-                  }}
-                  onClick={(e) => {
-                    // @ts-expect-error
-                    player.current.toggle(e);
+                    position: "relative",
                   }}
                 >
-                  {playing ? null : (
-                    <div
-                      style={{
-                        width: 200,
-                        height: 200,
-                        backgroundColor: "white",
-                        borderRadius: "50%",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        flexDirection: "column",
-                        boxShadow:
-                          "0 0 40px " + transparentize(0.9, BASE_COLOR),
-                      }}
-                    >
-                      <svg
-                        style={{
-                          height: 60,
-                          transform: `translateX(3px)`,
-                        }}
-                        viewBox="0 0 448 512"
-                      >
-                        <path
-                          fill={BASE_COLOR}
-                          d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"
-                        ></path>
-                      </svg>
-                      <br />
+                  <Player
+                    ref={player}
+                    component={Main}
+                    compositionHeight={1080}
+                    compositionWidth={1080}
+                    durationInFrames={990}
+                    fps={30}
+                    style={{
+                      ...layout,
+                      boxShadow: "0 0 10px " + transparentize(0.8, BASE_COLOR),
+                      borderRadius: 10,
+                      overflow: "hidden",
+                    }}
+                    inputProps={{
+                      stats: user,
+                      enableDecoration: false,
+                    }}
+                  ></Player>
+                  <AbsoluteFill
+                    style={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexDirection: "column",
+                      display: "flex",
+                      cursor: "pointer",
+                    }}
+                    onClick={(e) => {
+                      // @ts-expect-error
+                      player.current.toggle(e);
+                    }}
+                  >
+                    {playing ? null : (
                       <div
                         style={{
-                          color: BASE_COLOR,
-                          fontFamily: "Jelle",
-                          textTransform: "uppercase",
-                          fontSize: 18,
+                          width: 200,
+                          height: 200,
+                          backgroundColor: "white",
+                          borderRadius: "50%",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          flexDirection: "column",
+                          boxShadow:
+                            "0 0 40px " + transparentize(0.9, BASE_COLOR),
                         }}
                       >
-                        Click to play
+                        <svg
+                          style={{
+                            height: 60,
+                            transform: `translateX(3px)`,
+                          }}
+                          viewBox="0 0 448 512"
+                        >
+                          <path
+                            fill={BASE_COLOR}
+                            d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"
+                          ></path>
+                        </svg>
+                        <br />
+                        <div
+                          style={{
+                            color: BASE_COLOR,
+                            fontFamily: "Jelle",
+                            textTransform: "uppercase",
+                            fontSize: 18,
+                          }}
+                        >
+                          Click to play
+                        </div>
                       </div>
-                    </div>
-                  )}
-                </AbsoluteFill>
-              </div>
-            ) : null}
-            <div
-              style={{
-                height: 40,
-              }}
-            ></div>
-            <div style={layout}>
-              <p
+                    )}
+                  </AbsoluteFill>
+                </div>
+              ) : null}
+              <div
                 style={{
-                  color: BASE_COLOR,
-                  fontFamily: "Jelle",
-                  textAlign: "center",
+                  height: 40,
                 }}
-              >
-                Download your video and tweet it using{" "}
-                <span
-                  style={{
-                    color: "black",
-                  }}
-                >
-                  #GithubWrapped
-                </span>{" "}
-                hashtag!
-              </p>
-              <Download
-                initialProgress={progress}
-                bucketName={bucketName}
-                renderId={renderId}
-                username={username}
-                functionName={props.functionName}
-                region={props.region}
-              ></Download>
-              {iosSafari() ? (
+              ></div>
+              <div style={layout}>
                 <p
                   style={{
                     color: BASE_COLOR,
                     fontFamily: "Jelle",
                     textAlign: "center",
-                    fontSize: 12,
                   }}
                 >
-                  Tip for iOS Safari: Long press the {'"'}Download button{'"'},
-                  then press {'"'}Download Linked File{'"'} to save the video
-                  locally.
+                  Download your video and tweet it using{" "}
+                  <span
+                    style={{
+                      color: "black",
+                    }}
+                  >
+                    #GithubWrapped
+                  </span>{" "}
+                  hashtag!
                 </p>
-              ) : null}
-              <br></br>
-              <Link href="/" passHref>
-                <button style={backButton}>View for another user</button>
-              </Link>
-              <br />
-              <br />
-              <br />
-            </div>
-          </header>
+                <Download
+                  initialProgress={progress}
+                  bucketName={bucketName}
+                  renderId={renderId}
+                  username={username}
+                  functionName={props.functionName}
+                  region={props.region}
+                ></Download>
+                {iosSafari() ? (
+                  <p
+                    style={{
+                      color: BASE_COLOR,
+                      fontFamily: "Jelle",
+                      textAlign: "center",
+                      fontSize: 12,
+                    }}
+                  >
+                    Tip for iOS Safari: Long press the {'"'}Download button{'"'}
+                    , then press {'"'}Download Linked File{'"'} to save the
+                    video locally.
+                  </p>
+                ) : null}
+                <br></br>
+                <Link href="/" passHref>
+                  <button style={backButton}>View for another user</button>
+                </Link>
+                <br />
+                <br />
+                <br />
+              </div>
+            </header>
+          </div>
         </div>
+        <Footer></Footer>
       </div>
-      <Footer></Footer>
-    </div>
+    </SafeHydrate>
   );
 }
